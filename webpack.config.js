d18b2736e940c82ba18e86c085a12664f6faa830
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2022-11-14 16:11:55 -0600

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: 'dist',
+        port: 2000
+    }
+}
